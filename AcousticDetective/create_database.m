c3564a9d3f68474d4e52751d0f19603d7bad92a0
Mohtasim-Jamee <mohtasimjamee3@gmail.com>
2025-10-20 03:16:32 +0600

    v1.0

--- AcousticDetective/create_database.m
+++ AcousticDetective/create_database.m
@@ -1,14 +1,34 @@
 %% clearing the workspace
 clc, clearvars, close all;
 
-%% checking for dependencies
+%% getting all the song files
 
-try 
-    ver('signal');
-    ver('image');
-catch
-    error("The script requires signal and image processign toolbox");
+data_dir = "data";
+database = containers.Map("KeyType","char", "ValueType", "any");
+
+%find all .mp3 files
+disp("Looking for the songs in the folder...");
+song_files = dir(fullfile(data_dir,"*.mp3"));
+if isempty(song_files)
+    error("No songs found!");
 end
 
-%% configuring the parameters
+num_songs = length(song_files);
+fprintf("Found %d songs in the database\n", num_songs);
 
+% Get the struct for the first song
+first_song = song_files(1); 
+% Combine its folder and name to get the full path
+full_path = fullfile(first_song.folder, first_song.name);
+
+fprintf("Now playing: %s\n", full_path);
+
+% Read the audio file
+[song, fs] = audioread(full_path);
+
+% --- Fix 2 & 3: Use correct variable (fs) and add play() ---
+% Create the player
+player = audioplayer(song, fs); 
+
+% Play the song
+play(player);
